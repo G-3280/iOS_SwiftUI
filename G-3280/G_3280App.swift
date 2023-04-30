970de1946f2a79a915e08e57d1f0ed2ae7d6f714
Genesis <dmsgktn1685@naver.com>
2023-04-30 14:51:54 +0900

    Initial Commit

---
+++ G-3280/G_3280App.swift
@@ -0,0 +1,17 @@
+//
+//  G_3280App.swift
+//  G-3280
+//
+//  Created by ParkJunHyuk on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct G_3280App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
